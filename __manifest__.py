e7e8fc72d3160c125bbe32d4f8d03f3004430f60
Kjel Delaey <kjel_delaey@hotmail.com>
2017-08-12 15:33:09 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Contact Website',
+    'description': 'Extension for the contact module',
+    'summary': 'Make it possible to open the website in a new tab',
+    'author': 'Kjel Delaey',
+    'version': '1.0.0',
+    'website': 'https://github.com/trimentor/odoo-contact-website',
+    'depends': ['base'],
+    'data': [
+        'views/contact_website.xml'
+    ],
+    'test': [],
+    'installable': True,
+    'application': False
+}
